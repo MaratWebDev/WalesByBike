227572b1ec097b709ec2bb2932502b7f28a602b6
Marat <maratfaizov83@gmail.com>
2015-04-06 18:49:22 +0100

    Updated CSS to SaSS, Changed banner and header, Styled Google Map and links on About page

--- js/nav1.js
+++ js/nav1.js
@@ -1,10 +1,59 @@
-$( document ).ready(function() {
+$( document ).ready(function () {
     var $menu = $('.nav-collapse'),
     $menulink = $('.nav-toggle');
     
-    $menulink.click(function(){
+    $menulink.click(function () {
         $menulink.toggleClass('active');
         $menu.toggleClass('active');
         return false;
     });
 });
+
+//Styling Google Maps
+
+window.onload = function () {
+
+
+    var latlng = new google.maps.LatLng(53.3193766, -3.8239764);
+
+    var styles = [
+        {
+            featureType: "landscape",
+            stylers: [
+                { color: '#fff' }
+            ]
+        }, {
+            featureType: "natural",
+            stylers: [
+                { hue: '#a6cadd' }
+            ]
+        }, {
+            featureType: "road",
+            stylers: [
+                { hue: '#0875C7' },
+                { saturation: -70 }
+            ]
+        }, {
+            featureType: "building",
+            elementType: "labels",
+            stylers: [
+                { hue: '#0875C7' }
+            ]
+        }, {
+            featureType: "poi", //points of interest
+            stylers: [
+                { hue: '#0875C7' }
+            ]
+        }
+    ];
+
+    var myOptions = {
+        zoom: 14,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        styles: styles
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), myOptions);
+}
